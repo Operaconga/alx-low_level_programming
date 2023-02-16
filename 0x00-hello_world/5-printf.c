c2673e724e9f6ce8242cb6035e4585a610303b26
Operaconga <operahub39@gmail.com>
2023-02-16 04:57:03 -0800

    update

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
 /**
- *main - This prints  with proper grammar, but the outcome is a piece of art,
+ * main - This prints with proper grammar, but the outcome is a piece of art,
  *
- * Return: This return 0
+ * Return: This returns 0
  */
-
 int main(void)
 {
-  printf("with proper grammar, but the outcome is a piece of art,\n");
-  return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
