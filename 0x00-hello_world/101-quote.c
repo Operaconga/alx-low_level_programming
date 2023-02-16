531714a04c20541ca348e0ae11bc8db3bdf0c228
Operaconga <operahub39@gmail.com>
2023-02-16 05:14:53 -0800

    update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,12 @@
 #include <unistd.h>
 
 /**
- * main - This prints and that piece of art is useful" - Dora Korpar, 2015-10-19
+ * main - This prints and that piece of art is userful" - Dora Korpar 2015-10-1
  * Return: This returns 1
  *
  */
 int main(void)
 {
-  write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" 59);
-  return (1);
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }
