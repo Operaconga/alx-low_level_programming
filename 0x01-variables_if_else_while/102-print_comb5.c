4938e8b0da05647640be4d72090382246208c231
Operaconga <operahub39@gmail.com>
2023-02-17 13:14:09 -0800

    update

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,29 +1,38 @@
-#include <stdio.h>
-
+#include <stdio.h>>
 /**
  *main - receives no argument
  *
  *Return: Always 0 (success)
  */
-
 int main(void)
 {
-	int i, j;
 
-	for (i = 0; i <= 99; i++)
-	{
-		for (j = i + 1; j <= 99; j++)
-		{
-			putchar('0' + (i / 10));
-			putchar('0' + (i % 10));
-			putchar(' ');
-			putchar('0' + (j / 10));
-			putchar('0' + (j % 10));
+int i, j;
+int a, b, c, d;
+for (i = 0; i < 100; i++)
+{
+a = i / 10;
+b = i % 10;
+for (j = 0; j < 100; j++)
+{
+c = j / 10;
+d = j % 10;
+if (a < c || (a == c && b < d))
+{
+putchar(a + '0');
+putchar(b + '0');
+putchar(32);
+putchar(c + '0');
+putchar(d + '0');
+if (!(a == 9 && b == d))
+{
+putchar(44);
+putchar(32);
+}
 
-			putchar(',');
-			putchar(' ');
-		}
-	}
-	putchar('\n');
-	return (0);
+}
+
+}
+}
+putchar(10);
 }
