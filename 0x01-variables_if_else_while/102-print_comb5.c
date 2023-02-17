b6639b45d505aeff2424644911871c8cfca71e49
Operaconga <operahub39@gmail.com>
2023-02-17 12:07:10 -0800

    update

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,8 +14,16 @@ int main(void)
 	{
 		for (j = i + 1; j <= 99; j++)
 		{
-		        putchar("%d %d/n", i,j);
+			putchar('0' + (i / 10));
+			putchar('0' + (i % 10));
+			putchar(' ');
+			putchar('0' + (j / 10));
+			putchar('0' + (j % 10));
+
+			putchar(',');
+			putchar(' ');
 		}
 	}
+	putchar('\n');
 	return (0);
 }
