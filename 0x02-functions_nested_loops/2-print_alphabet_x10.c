4d2c59b395dac208f98b7aaf66a8d9829ef5a5a1
Operaconga <operahub39@gmail.com>
2023-02-21 07:21:26 -0800

    update

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,19 +1,21 @@
 #include "main.h"
-/**
- * print_alphabet_x10 - print all alphbets ten times
- *
- * Return: void
- */
-int print_alphabet_x10(void)
-{
-	char counter, lower;
 
-	for (counter = '0'; counter <= '9'; counter++)
+/**
+ * print_alphabet_x10 - prints lowercase alphabets ten times
+ *
+ * Return: 0
+ */
+
+void print_alphabet_x10(void)
+{
+	char count, lower;
+
+	for (count = '0'; count <= '9'; count++)
 	{
-		for (lower = 'a'; lower <='z'; lower++)
+		for (lower = 'a'; lower <= 'z'; lower++)
 		{
 			_putchar(lower);
 		}
-		_putchar('\n');
+	_putchar('\n');
 	}
 }
