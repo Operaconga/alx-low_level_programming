09c2920e7aad6461469f7401fc4017f4ddd8ce15
Operaconga <operahub39@gmail.com>
2023-02-21 11:26:14 -0800

    update

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isalpha - checks for alphabet character
+ *
+ * Return: Always 1 (success)
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
