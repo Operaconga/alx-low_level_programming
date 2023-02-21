d1821a45a9eec3373d631f306eb89a5f30b961da
Operaconga <operahub39@gmail.com>
2023-02-21 04:16:52 -0800

    alpha

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet - prints alphabet in lowwer case followed by a new line
+ *
+ * Return: Always 0 (success)
+ */
+int print_alphabet(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z' alpha++)
+	{
+		_putchar(alpha);
+	}
+
+	_putchar('\n');
+
+	return (0);
+}
