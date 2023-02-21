1e661ab5ab725a9269553590acdb408c0a4961d6
Operaconga <operahub39@gmail.com>
2023-02-21 12:01:00 -0800

    nest

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,10 +1,14 @@
+
+
 #include "main.h"
+
 /**
- * print_sign - prints the sign of a number
- *
- * Return: 1
+ * print_sign - to check for a sign of a number
+ * @n:  is the int that will use for the argument of the function
+ * Return: 0
  */
-int print_sign(int n);
+
+int print_sign(int n)
 {
 	if (n > 0)
 	{
