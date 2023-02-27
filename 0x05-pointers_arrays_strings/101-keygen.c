69e322506591f09fa68e7879eb44eb95f900edde
Operaconga <operahub39@gmail.com>
2023-02-27 12:18:34 -0800

    keygen

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,12 +1,49 @@
 #include "main.h"
 #include <stdio.h>
+#include <time.h>
 
 /**
  * keygen - a function that generates a random valid password
  */
-
-int keygen(int password)
+void keygen(int N)
 {
-	printf(password);
-	printf("\n");
+	int i = 0;
+	int randomizer = 0;
+	srand((unsigned int) (time(NULL)));
+
+	char numbers[] = "0123456789";
+	char letters[] = "abcdefghijklmnopqrstuvwxyz";
+	char symbols[] = "@#$^&*?";
+
+	char password[N];
+
+	randomizer = rand() % 4;
+
+	for (i = 0; i < N; i++)
+	{
+		if (randomizer == 1)
+		{
+			password[i] = numbers[rand() % 10];
+			randomizer = rand() % 4;
+			printf("%c", password[i]);
+		}
+		else if (randomizer == 2)
+		{
+			password[i] = symbols[rand() % 10];
+			randomizer = rand() % 4;
+			printf("%c", password[i]);
+		}
+		else if (randomizer[i] = 3)
+		{
+			password[i] = letters[rand() % 8];
+			randomizer = rand() % 4;
+			printf("%c", password[i]);
+		}
+		else
+		{
+			password[i] = letters[rand() % 26];
+			randomizer = rand() % 4;
+			printf("%c", password[i]);
+		}
+	}
 }
