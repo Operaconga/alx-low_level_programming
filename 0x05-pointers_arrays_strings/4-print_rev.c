ff6a71df148e45c2361aace9bc10e3478e77baa1
Operaconga <operahub39@gmail.com>
2023-02-27 11:05:18 -0800

    added

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,19 +1,19 @@
 #include "main.h"
 
 /**
- * print_rev  - check the string in reverse followed by new line
- *@s is the parameter
+ *print_rev - prints a string in reverse
+ *@s: is the parameter
  */
 
-void print_rev(char *s);
+void print_rev(char *s)
 {
 	int c;
 
-	for (c =0; s[c] !=0; c++)
+	for (c = 0; s[c] != 0; c++)
 	{
 	}
 	for (c = c - 1; c >= 0; c--)
-	{
 		_putchar(s[c]);
-		_putchar('\n');
-	}
+	_putchar('\n');
+
+}
