2d1b340df53a14606870353e446e2a152ff6e580
Operaconga <operahub39@gmail.com>
2023-03-06 18:27:52 -0800

    update

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,22 +1,25 @@
 #include "main.h"
-
 /**
- *_memcpy copies memory area
- *@dest is where memory arear is copied to
- *@src is where momory area copied from
- *@n is a copied bytes
- *
+ *_memcpy -   copy number bytes form adress "from" to adress "to"
+ *@dest: content is to be copied
+ *@src: Source of data to be copied
+ *@n: bytes of the memory
  *Return: dest
  */
-char *_memcpy(char *dest, char *src, unsigned int n);
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+
 {
 	unsigned int i;
 
 	i = 0;
-	while(i < n)
+	while (i < n)
 	{
-		*(dest + i) = * *(src + i);
+		*(dest + i) = *(src + i);
 		i++;
+
 	}
+
+
 	return (dest);
 }
