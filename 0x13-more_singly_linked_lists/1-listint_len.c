25a2fb352247295f495841c2cde277ef1ba801be
Operaconga <operahub39@gmail.com>
2023-04-03 12:41:19 -0700

    initial

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,21 @@
+#include "list.h"
+
+/**
+ * listint_len - return the number of elements in
+ * a linked list
+ *
+ * Return: numbers of nodes.
+ */
+size_t listint_len(const listint_t *h)
+{
+
+	size_t nnodes = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		nnodes++;
+	}
+	return (nnodes);
+}
+
