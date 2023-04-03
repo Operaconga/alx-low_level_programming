c3e23e817fd35b335abe41507f3e4cd72a264748
Operaconga <operahub39@gmail.com>
2023-04-03 14:15:13 -0700

    correction

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,11 +1,11 @@
-#include "list.h"
+#include "lists.h"
 #include <stdio.h>
 
 /**
  * print_listint - Prints all the elements of a listint_t list.
  * @h: A pointer to the head of the list_t list.
  *
- * Return: The number of nodes in the list_t list
+ * Return: The number of nodes of the list_t list.
  */
 size_t print_listint(const listint_t *h)
 {
@@ -14,10 +14,9 @@ size_t print_listint(const listint_t *h)
 	while (h)
 	{
 		nodes++;
-		printf("%\n", h->n);
+		printf("%d\n", h->n);
 		h = h->next;
 	}
 
 	return (nodes);
-
 }
