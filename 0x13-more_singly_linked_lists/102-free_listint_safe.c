e958aec4bb2ab1389a877a3845e9813a22782394
Operaconga <operahub39@gmail.com>
2023-04-04 10:37:22 -0700

    update

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -5,16 +5,16 @@ size_t free_listint_safe(listint_t **h);
 
 /**
  * looped_listint_count - Counts the number of unique nodes
- *				in a looped listint_t linked list.
+ *                      in a looped listint_t linked list.
  * @head: A pointer to the head of the listint_t to check.
  *
- * Return: If the list is not looped - 0,
- *		Otherwise - the number of the unique node in the list.
+ * Return: If the list is not looped - 0.
+ *         Otherwise - the number of unique nodes in the list.
  */
 size_t looped_listint_count(listint_t *head)
 {
 	listint_t *tortoise, *hare;
-	size_t node = 1;
+	size_t nodes = 1;
 
 	if (head == NULL || head->next == NULL)
 		return (0);
