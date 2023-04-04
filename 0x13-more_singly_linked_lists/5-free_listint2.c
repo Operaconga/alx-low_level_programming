cfdae68a0de0fc7eb2875415cf935aabe6367561
Operaconga <operahub39@gmail.com>
2023-04-04 03:20:12 -0700

    adde

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,17 +1,17 @@
 #include "lists.h"
 
 /**
-  * free_listint2 - Frees a listint_t list.
-  * @head: A pointer to the address of the 
-  *	   head of the listint_t list.
-  *
-  * Description: Sets the head to NULL.
-  */
-void free_lintint2(listint_t **head)
+ * free_listint2 - Frees a listint_t list.
+ * @head: A pointer to the address of the
+ *        head of the listint_t list.
+ *
+ * Description: Sets the head to NULL.
+ */
+void free_listint2(listint_t **head)
 {
 	listint_t *tmp;
 
-	if (heap == NULL)
+	if (head == NULL)
 		return;
 
 	while (*head)
