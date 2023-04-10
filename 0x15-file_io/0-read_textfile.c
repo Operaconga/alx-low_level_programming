7b795dbef35413169c15c7a0d1fbde94c573ae9c
Operaconga <operahub39@gmail.com>
2023-04-10 01:52:18 -0700

    initial

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * read_textfile - Read text file and print POSIX standard output.
+ * @filename: textfile being read.
+ * @letters: number of letters to read.
+ * Return: actual number of letters it could read and print
+ *		Otherwise 0 when file can not be opened or read.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buff;
+	ssize_t op, rd, wt;
+
+	op = open(filename, 0_RDONLY);
+	if (op == -1)
+		return (0);
+	buff = malloc(sizeof(char) * letters);
+	rd = read(op, buff, letters);
+	wt = write(STDOUT_FILENO, buff, rd);
+
+	free(buff);
+	close(op);
+	return (wt);
+}
