6db1fadc6a4aa66544d54cb56571487ee084a63c
Operaconga <operahub39@gmail.com>
2023-04-10 14:39:50 -0700

    changes

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,14 +10,17 @@
  * Return: Bit at index
  */
 
-int get_bit(unsigned long int n, usigned int index)
+int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned long int mask = 1;
+
 	if (index > sizeof(n) * 8)
 	{
 		return (-1);
 	}
+
 	mask <<= index;
+
 	if (mask & n)
 		return (1);
 	else
