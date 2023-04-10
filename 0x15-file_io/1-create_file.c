8570aa3656790c6f0f76552d80d7e9f28e5f40ac
Operaconga <operahub39@gmail.com>
2023-04-10 03:05:36 -0700

    changes

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * create_file - Create a file.
+ * create_file - Creates a file.
  * @filename: A pointer to the name of the file to create.
  * @text_content: A pointer to a string to write to the file.
  *
  * Return: If the function fails - -1.
- *		Otherwise - 1.
+ *         Otherwise - 1.
  */
 int create_file(const char *filename, char *text_content)
 {
@@ -21,8 +21,8 @@ int create_file(const char *filename, char *text_content)
 			len++;
 	}
 
-	o = open(filename, O_CREATE | O_RDWR | O_TRUNC. 0600);
-	w = write(0, text_content, len);
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(o, text_content, len);
 
 	if (o == -1 || w == -1)
 		return (-1);
